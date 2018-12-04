0f47555657bb25d5dbe26023455d7f0f3d990da0
thentges <thibault.hentges@gmail.com>
2018-12-04 15:58:22 +0100

    admin +++

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -29,13 +29,6 @@ class LoginController extends Controller
     }
 
     /**
-     * Where to redirect users after login.
-     *
-     * @var string
-     */
-    // protected $redirectTo = '/profile';
-
-    /**
      * Create a new controller instance.
      *
      * @return void
