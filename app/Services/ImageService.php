023ca2239de9517351dd724080203b5e78f1d651
Hafizh Ahmad Zuhdi,Hafizh H <h.zuhdi@student.fontys.nl>
2018-12-16 15:39:42 +0100

    Add carousel in homepage

--- app/Services/ImageService.php
+++ app/Services/ImageService.php
@@ -1,11 +1,17 @@
 <?php
+
+use Intervention\Image\ImageManagerStatic as Image;
 namespace App\Services;
+
 
 class ImageService {
 
     public function getFileNameFromRequestAndSaveIt($file_from_request){
         $filename = $file_from_request->getClientOriginalName();
-        $file_from_request->move(public_path()."/images/", $filename);
+        $image_resize = Intervention\Image\ImageManagerStatic::make($file_from_request->getRealPath());
+        $image_resize->resize(300, 300);
+        $image_resize->save(public_path('/images/' .$filename));
+        /*$file_from_request->move(public_path()."/images/", $filename);*/
         return $filename;
     }
 
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,6 +1,54 @@
 @extends('template.index')
 @section('content')
 
+
+<!-- Part for carousell -->
+      <section class="site-section py-sm" style="margin-top: 10px;">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-12">
+
+            <div class="owl-carousel owl-theme home-slider">
+              
+              <?php $count = 0; ?>
+              @foreach($blogs as $b)
+              <?php if($count == 3) break; ?>
+                    <div>
+                        @if ($b->image)
+                            <a href="{{ url('read', $b->id) }}" class="a-block d-flex align-items-center height-lg" style="background-image: url('{{ asset('images/'.$b->image)  }}'); ">
+                        @else
+                             <a href="{{ url('read', $b->id) }}" class="a-block d-flex align-items-center height-lg" style="background-image: url('images/img_1.jpg'); ">
+                        @endif
+                      
+                      <div class="text half-to-full">
+                      <div class="post-meta">
+                        <span class="category">{{$b->countries}}</span>
+                        <span class="mr-2">{{$b->created_at}}</span> &bullet;
+                        <span class="ml-2"><span class="fa fa-comments"></span> {{$b->comments->count()}}</span>
+                    </div>
+                    <h3>{{$b->title}}</h3>
+                    <p>{!!substr($b->content,0,50)!!}...</p>
+                  </div>
+                </a>
+              </div>
+              <?php $count++; ?>
+              @endforeach
+            </div>
+            
+          </div>
+        </div>
+      </div>
+    </section>
+<!-- End Carousel -->
+
+      <div class="text-center" style="font-size: 25px;">
+        <br />
+        <strong><i>"All you have to do is sharing and caring"</i></strong> - John Lennon
+        <br />
+      </div>
+
+
+<!-- Blog card -->
 @if(count($blogs) > 0)
 
 <section class="site-section py-sm">
@@ -42,4 +90,8 @@
 
 @endif
 
+<!-- End of blogcard -->
+
 @stop
+
+
