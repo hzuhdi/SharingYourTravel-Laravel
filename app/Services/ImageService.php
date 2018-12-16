47b460653439ba86b4323d2af7c0a63300aec64b
Hafizh Ahmad Zuhdi,Hafizh H <h.zuhdi@student.fontys.nl>
2018-12-16 15:50:39 +0100

    Rollback imgservice, ToDo use intervention

--- app/Services/ImageService.php
+++ app/Services/ImageService.php
@@ -7,11 +7,18 @@ namespace App\Services;
 class ImageService {
 
     public function getFileNameFromRequestAndSaveIt($file_from_request){
-        $filename = $file_from_request->getClientOriginalName();
+        /*$filename = $file_from_request->getClientOriginalName();
         $image_resize = Intervention\Image\ImageManagerStatic::make($file_from_request->getRealPath());
         $image_resize->resize(300, 300);
-        $image_resize->save(public_path('/images/' .$filename));
+        $image_resize->save(public_path('/images/' .$filename));*/
         /*$file_from_request->move(public_path()."/images/", $filename);*/
+
+        /*TO DO up here to use intervention to resizing the image*/
+
+
+        $filename = $file_from_request->getClientOriginalName();
+        $file_from_request->move(public_path()."/images/", $filename);
+        //$file_from_request->resize(300, 300);
         return $filename;
     }
 
