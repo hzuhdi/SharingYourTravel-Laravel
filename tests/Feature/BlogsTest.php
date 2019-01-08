31c72c3e52f4dfa2b786271b7f4e70a1072ff052
thentges <thibault.hentges@gmail.com>
2019-01-08 19:20:21 +0100

    securing DELETE route for blogs

--- tests/Feature/BlogsTest.php
+++ tests/Feature/BlogsTest.php
@@ -129,7 +129,55 @@ class BlogsTest extends TestCase
             'id' => $blog->id
         ]);
 
+        // with no user
         $response = $this->json('DELETE', '/api/blogs/'.$blog->id);
+        $response->assertStatus(401);
+        $this->assertDatabaseHas('blogs', [
+            'id' => $blog->id
+        ]);
+
+        // with non owner
+        $random_user = factory(\App\User::class)->create();
+        $token = JWTAuth::fromUser($random_user);
+        $response = $this->withHeaders([
+            'Authorization' => 'Bearer ' . $token])
+        ->json('DELETE', '/api/blogs/'.$blog->id);
+        $response->assertStatus(401);
+        $this->assertDatabaseHas('blogs', [
+            'id' => $blog->id
+        ]);
+
+        // with owner
+        $token = JWTAuth::fromUser($blog->user);
+        $response = $this->withHeaders([
+            'Authorization' => 'Bearer ' . $token])
+        ->json('DELETE', '/api/blogs/'.$blog->id);
+        $response->assertStatus(200)->assertJson([
+                'id' => $blog->id,
+        ]);
+
+        $this->assertDatabaseMissing('blogs', [
+            'id' => $blog->id
+        ]);
+
+        // with admin
+        $blog = factory(\App\Blog::class)->create([
+            'user_id' => function() {
+                // create a User, because a blog need a user to exist
+                $user = factory(\App\User::class)->create();
+                return $user->id;
+            }
+        ]);
+
+        $this->assertDatabaseHas('blogs', [
+            'id' => $blog->id
+        ]);
+
+        $admin = factory(\App\User::class)->create(['type' => 'admin']);
+        $token = JWTAuth::fromUser($admin);
+        $response = $this->withHeaders([
+            'Authorization' => 'Bearer ' . $token])
+        ->json('DELETE', '/api/blogs/'.$blog->id);
         $response->assertStatus(200)->assertJson([
                 'id' => $blog->id,
         ]);
